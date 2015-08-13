ad79b206be3b13d58e41abaddc1c8e87ec1a4815
Lukas WP <lukaswinklerprins@gmail.com>
2015-08-13 21:20:42 +0200

    Getting a Case class set up.

---
+++ graph.py
@@ -0,0 +1,49 @@
+class Case(object):
+  """Represents one Supreme Court Case."""
+
+  def __init__(self,year=1000,id_num=0,citations=[]):
+    self.year = year
+    self.id_num = id_num
+    self.citations = citations
+
+  def __str__(self):
+    print 'Case no. %s from %s. It cites the following:' % (self.id_num, self.year)
+    print self.citations
+    return ''
+
+cases = dict()
+
+fin = open('ussc_cites_small.txt')
+for line in fin:
+  line = line.strip();
+  line = line.split(',')
+  # CITED, CITING
+  cited = line[0].split()
+  citing = line[1].split()
+  # 0th is year, 3th is id_num
+
+  if citing[3] not in cases:
+    # we're gonna make an object  for the citing case and put it in our dictionary
+    case = Case(citing[0],citing[3],[cited[3]])
+    cases[citing[3]] = case
+  else:
+    case = cases[citing[3]]
+    case.citations.append(cited[3])
+
+#################################################################
+
+import networkx as nx
+G = nx.DiGraph()
+
+for c in cases:
+  #print cases[c].id_num
+
+  G.add_node(cases[c].id_num)
+  # Uses case numbers as nodes. Maybe we should use case objects?
+
+  #print cases[c].citations
+  for k in cases[c].citations:
+    G.add_edge(cases[c].id_num,k)
+
+G.nodes()
+G.edges()
